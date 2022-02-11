ec97e89cb50019141a5c21ef1785cde844f1f32c
Mitja Krašovec <mitja.krasovec@specto.si>
2022-02-11 13:41:22 +0100

    started working on form validation

--- source/main.js
+++ source/main.js
@@ -1,2 +1,18 @@
-let d = new Date;
-document.body.innerHTML = "<h4>Today's date is: " + d + "</h4>"
+class FormValidator {
+    constructor(form, fields) {
+        this.form = form;
+        this.fields = fields;
+    }
+
+    initialize() {
+        console.log('form', this.form)
+        console.log('fields', this.fields)
+    }
+}
+
+const form = document.querySelector('.form')
+const fields = ["username", "email", "password", "confirmPassword"];
+
+const validator = new FormValidator(form, fields)
+
+validator.initialize();
