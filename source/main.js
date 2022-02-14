830aec1fc983ecf3e77c6f02d1017034399e4155
Mitja Krašovec <mitja.krasovec@specto.si>
2022-02-14 10:09:45 +0100

    Password requirements added & validation

--- source/main.js
+++ source/main.js
@@ -1,13 +1,37 @@
-var errorNumber = document.getElementById("needsNumber");
-var errorUppercase = document.getElementById("needsUppercase");
-var errorSymbol = document.getElementById("needsSymbol");
+const passwordInput = document.querySelector("#password");
+const strongPass = new RegExp('^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$');
+
+const errorMessage = [
+    {
+        testProperty: new RegExp('(?=.*[0-9])'),
+        errorElement: document.querySelector("#needsNumber")
+    },
+    {
+        testProperty: new RegExp('(?=.*[A-Z])'),
+        errorElement: document.querySelector("#needsUppercase")
+    },
+    {
+        testProperty: new RegExp('(?=.*[a-z])'),
+        errorElement: document.querySelector("#needsLowercase")
+    },
+    {
+        testProperty: new RegExp('(?=.*[!@#$&*])'),
+        errorElement: document.querySelector("#needsSymbol")
+    },
+    {
+        testProperty: new RegExp('.{8,}'),
+        errorElement: document.querySelector("#needsEnoughChars")
+    }
+]
 
 function ErrorCheck() {
-    var passwordInput = document.getElementById("password");
-    
-    console.log(passwordInput.value);
-}
 
-errorNumber.classList.add("hiddenItem");
-errorUppercase.classList.add("hiddenItem");
-errorSymbol.classList.add("hiddenItem");
+    errorMessage.forEach(({ errorElement, testProperty }) => {
+        errorElement.classList.toggle("hiddenItem", testProperty.test(passwordInput.value));
+    })   
+
+
+    if(strongPass.test(passwordInput.value) === true) {
+        console.log('Allow User to send!')
+    }
+}
