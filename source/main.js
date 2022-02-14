c6f9e2ad85fc67c0eba933eae336cd00d12d6104
Mitja Krašovec <mitja.krasovec@specto.si>
2022-02-14 10:48:03 +0100

    additional verifications added

--- source/main.js
+++ source/main.js
@@ -1,5 +1,20 @@
 const passwordInput = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+const emailInput = document.querySelector("#email");
+
 const strongPass = new RegExp('^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$');
+const emailValidate = new RegExp('^[^\s@]+@[^\s@]+\.[^\s@]+$');
+const matchPasswordsError = document.querySelector("#mustMatchPassword");
+
+const submitBtn = document.querySelector('#submitBtn')
+
+submitBtn.disabled = true;
+
+const submitConditions = [
+    okPassword = false,
+    passwordMatch = false,
+
+]
 
 const errorMessage = [
     {
@@ -24,14 +39,31 @@ const errorMessage = [
     }
 ]
 
-function ErrorCheck() {
+function EmailCheck() {
+    console.log(emailInput.value)
+    console.log(emailValidate.test(emailInput.value));
+}
+
+function PasswordCheck() {
 
     errorMessage.forEach(({ errorElement, testProperty }) => {
         errorElement.classList.toggle("hiddenItem", testProperty.test(passwordInput.value));
-    })   
-
-
-    if(strongPass.test(passwordInput.value) === true) {
-        console.log('Allow User to send!')
+    })
+    if(strongPass.test(passwordInput.value)) {
+        console.log('Password is ok!')
     }
+}
+
+function confirmPasswords() {
+    matchPasswordsError.classList.toggle("hiddenItem", passwordInput.value === confirmPassword.value);
+
+    if (passwordInput.value === confirmPassword.value){
+        console.log("Passwords match!")        
+    }
+}
+
+function ShowErrors() {
+    errorMessage.forEach(({errorElement, testProperty}) => {
+        errorElement.classList.toggle("hiddenItem", testProperty.test(passwordInput.value));
+    })
 }
